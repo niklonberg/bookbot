f32855a127982b54651a65d45c075b06a178f0e0
Nikolai Lønberg <niklonberg@gmail.com>
2025-05-12 15:16:30 +0200

    move print_alphabet_characters fn from main, adjust print statement

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
-from stats import count_words, get_characters_count
+from stats import count_words, get_characters_count, print_alphabet_characters
 
 def main():
   with open("books/frankenstein.txt") as f:
     file_contents = f.read()
-    print(f"{count_words(file_contents)} words found in the document")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(file_contents)} total words")
     characters_dict = get_characters_count(file_contents)
     print_alphabet_characters(characters_dict)
     print("- End of report -")
-
-def print_alphabet_characters(character_dict):
-  char_list = []
-  for c in character_dict:
-    if (c.isalpha()):
-      char_list.append({
-        "char": c,
-        "count": character_dict[c]
-        })
-      
-  def sort_on(dict):
-    return dict["count"]
-  
-  char_list.sort(reverse=True, key=sort_on)
-  
-  for c in char_list:
-    print(f"'{c['char']}': {c['count']}")
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,21 @@ def get_characters_count(text):
       character_counts[lowercased] = 1
   
   return character_counts
+
+def print_alphabet_characters(character_dict):
+  char_list = []
+  for c in character_dict:
+    if (c.isalpha()):
+      char_list.append({
+        "char": c,
+        "count": character_dict[c]
+        })
+      
+  def sort_on(dict):
+    return dict["count"]
+  
+  char_list.sort(reverse=True, key=sort_on)
+  
+  print("--------- Character Count -------")
+  for c in char_list:
+    print(f"{c['char']}: {c['count']}")
