655a8b40981c80b839e074ef42b1cdb3bffced3c
Nikolai Lønberg <niklonberg@gmail.com>
2025-05-12 15:27:33 +0200

    remove hardcoded book paths, use sys.argv

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import sys
+
 from stats import count_words, get_characters_count, print_alphabet_characters
 
 def main():
-  with open("books/frankenstein.txt") as f:
+  if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+  book_path = sys.argv[1]
+
+  with open(book_path) as f:
     file_contents = f.read()
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {count_words(file_contents)} total words")
     characters_dict = get_characters_count(file_contents)
